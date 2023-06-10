58ce125d5d227e2968e5f688e31e29abb8b808bc
silopuedehacer12 <107371597+silopuedehacer12@users.noreply.github.com>
2023-06-09 19:43:53 -0500

    Add files via upload

---
+++ mira 1 carpeta y  el archivo arriba dije una torre con pizza pero vegana es la moda no pero las chavas quieren veganismo por que no soportan que un pollito de 2 dias lo matan y ya por eso le di crocetas a un perro/main.js
@@ -0,0 +1,23 @@
+
+var canvas = new fabric.Canvas('mycanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{ fabric.Image.fromURL("birthdayimage.jpg",function(Img){
+    block_image_object=Img;
+    block_image_object.scaleToWidth(700);
+    block_image_object.scaleToHeight(500);
+    block_image_object.set({
+        top:0,
+        left:0
+    });
+    canvas.add(block_image_object);
+
+});
+	
+	
+}
+
+function playsound(){
+x.play();	
+}
